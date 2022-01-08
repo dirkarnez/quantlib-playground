dd179ebf530a417beb605c31fe40c7bb33940efe
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-08 21:38:15 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -3,11 +3,18 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOAD_DIR=%USERPROFILE%\Downloads
 set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-posix;%DOWNLOAD_DIR%\mingw64-posix\bin;%DOWNLOAD_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin;%PATH%
-cmake.exe -G"MinGW Makefiles" -B./build
+SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin;
+cmake.exe -G"MinGW Makefiles" ^
+-DBUILD_SHARED_LIBS=OFF ^
+-DBOOST_ROOT="P:/Downloads/cpp-libraries/boost/lib/cmake/Boost-1.78.0" ^
+-DCMAKE_INSTALL_PREFIX="%~dp0build/QuantLib" ^
+-DQL_BUILD_BENCHMARK=OFF ^
+-DQL_BUILD_EXAMPLES=OFF ^
+-DQL_BUILD_TEST_SUITE=OFF ^
+-B./build
 cd build
 
 :rebuild_and_startapp
-mingw32-make.exe
+mingw32-make.exe install
 pause
 REM GOTO rebuild_and_startapp
