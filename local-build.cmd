1e90d2a6f7ae78c2cedd2161e185d2ead541a40e
dirkarnez <smalldirkalex@gmail.com>
2022-08-18 03:33:40 +0800

    - revamp project

--- local-build.cmd
+++ local-build.cmd
@@ -1,20 +1,26 @@
 REM run as Administrator
 @echo off
 cd /d %~dp0
-set DOWNLOAD_DIR=%USERPROFILE%\Downloads
-set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin;
-cmake.exe -G"MinGW Makefiles" ^
--DBUILD_SHARED_LIBS=OFF ^
--DBOOST_ROOT="P:/Downloads/cpp-libraries/boost/lib/cmake/Boost-1.78.0" ^
--DCMAKE_INSTALL_PREFIX="%~dp0build/QuantLib" ^
--DQL_BUILD_BENCHMARK=OFF ^
--DQL_BUILD_EXAMPLES=OFF ^
--DQL_BUILD_TEST_SUITE=OFF ^
--B./build
-cd build
+@REM set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+@REM set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
 
-:rebuild_and_startapp
-mingw32-make.exe install
+@REM SET PATH=^
+@REM %DOWNLOAD_DIR%\PortableGit\bin;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;
+
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+cmake.exe -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug ^
+-DQUANTLIB_ROOT="D:/Code Projects/cpp-libraries/quantlib-v1.27-x86_64-posix-seh-rev0-8.1.0" ^
+-DBOOST_ROOT="D:/Code Projects/cpp-libraries/boost-1.78.0-windows-2019-mingw-static" ^
+-B./build &&^
+cd build &&^
+cmake --build . &&^
+echo "Successful build"
 pause
-REM GOTO rebuild_and_startapp
+
